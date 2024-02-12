468136af44f332c3e586c10d5ea46405697e8b53
kaiomaced0 <kaiomm2000@gmail.com>
2024-02-12 03:10:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    themeToggle.addEventListener('click', function() {
+        body.classList.toggle('dark-theme');
+        body.classList.toggle('light-theme');
+    });
+});
